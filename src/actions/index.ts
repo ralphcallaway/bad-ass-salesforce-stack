20a38ec20ef6e79b1dd029587c53740104b83bca
Charles Jonas <charlie@callawaycloudconsulting.com>
2017-09-25 14:08:45 -0600

    added blank branch

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,16 +1,7 @@
 import { GlobalState } from "@src/reducers";
 import { ThunkAction } from "redux-thunk";
 
-export * from "./todo";
-export * from "./done";
-
 export enum TypeKeys {
-  ADD_TODO = "ADD_TODO",
-  REMOVE_TODO = "REMOVE_TODO",
-  LOAD_TODO = "LOAD_TODO",
-  ADD_DONE = "ADD_DONE",
-  REMOVE_DONE = "REMOVE_DONE",
-  LOAD_DONE = "LOAD_DONE",
   OTHER_ACTION = "__any_other_action_type__",
 }
 
--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,4 @@
-import {Counter} from "@src/components";
-import {Done, Todo} from "@src/containers";
-import { Card, Layout, LocaleProvider, Tabs } from "antd";
+import { Card, Layout, LocaleProvider } from "antd";
 import enUS from "antd/lib/locale-provider/en_US";
 import * as React from "react";
 
@@ -15,16 +13,8 @@ export class App extends React.Component<{}, {}> {
       <LocaleProvider locale={enUS}>
           <Layout>
             <Layout.Content>
-              <Card title="B.A.S.S. Starter">
-              <Tabs defaultActiveKey="1">
-                <Tabs.TabPane tab="COUNTER" key="1" >
-                     <Counter/>
-                 </Tabs.TabPane >
-                <Tabs.TabPane tab="TODO" key="2" >
-                  <Todo />
-                  <Done />
-                 </Tabs.TabPane >
-              </Tabs>
+              <Card title="Blank Starter">
+                I don't do anything :(
               </Card>
             </Layout.Content>
           </Layout>
