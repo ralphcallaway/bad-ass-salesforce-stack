537387510da6c4ece8b113d89d725102af9b9c30
Ralph Callaway <ralph@callawaycloudconsulting.com>
2018-03-28 16:17:45 -0600

    hello world

---
+++ src/app.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+export const App = () => {
+    return <h1>Hello, World!</h1>;
+};
---
+++ src/index.tsx
@@ -0,0 +1,48 @@
+// entry to allow for HMR.
+//  DON'T TOUCH THIS UNLESS YOU KNOW WHAT YOURE DOING
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { AppContainer } from "react-hot-loader";
+// import { Provider } from "react-redux";
+// import { Rest } from "ts-force";
+import { App } from "./app";
+// import createGlobalStore from "./store";
+
+// globals. set on page window
+declare var __RESTHOST__: string;
+declare var __ACCESSTOKEN__: string;
+declare var __ACCOUNTID__: string;
+
+// Rest.config = {
+//   accessToken: __ACCESSTOKEN__,
+//   instanceUrl:  __RESTHOST__,
+//   version: 40,
+// };
+
+// const store = createGlobalStore();
+
+ReactDOM.render(
+    <AppContainer>
+      {/* <Provider store={store}> */}
+        <App />
+      {/* </Provider> */}
+    </AppContainer>
+    ,
+    document.getElementById("root") as HTMLElement,
+);
+
+if (module.hot) {
+    module.hot.accept();
+
+    // reducers
+    // module.hot.accept("./reducers/index", () => {
+    //   const nextRootReducer = require("./reducers/index");
+    //   store.replaceReducer(nextRootReducer);
+    // });
+
+    // // epics
+    // module.hot.accept('../modules/root-epic', () => {
+    //   const newRootEpic = require('./root-epic').default;
+    //   epicMiddleware.replaceEpic(newRootEpic);
+    // });
+}
