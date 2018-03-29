40bdb5027803b92d3a8265c269d5efc300e2d7f0
Ralph Callaway <ralph@callawaycloudconsulting.com>
2018-03-29 03:20:04 -0600

    switch to redux with mocked actions

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 // entry to allow for HMR.
 //  DON'T TOUCH THIS UNLESS YOU KNOW WHAT YOURE DOING
+// tslint:disable-next-line:no-implicit-dependencies
+import { load, loaded } from "@src/actions";
+// tslint:disable-next-line:no-implicit-dependencies
+import { rootReducer } from "@src/reducers";
 import * as queryString from "query-string";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import { Rest } from "ts-force";
 import { App } from "./app";
-// import createGlobalStore from "./store";
 
 // globals. set on page window
 declare var __RESTHOST__: string;
@@ -18,17 +22,22 @@ Rest.config = {
   instanceUrl:  __RESTHOST__,
 };
 
-// const store = createGlobalStore();
+const store = createStore(rootReducer);
 
 // load url params
 const params = queryString.parse(location.search);
-const id = params.id || "";
+if (params.id) {
+    store.dispatch(load(params.id));
+    setTimeout(() => {
+        store.dispatch(loaded("My Opp Name", 5));
+    }, 2000);
+}
 
 ReactDOM.render(
     <AppContainer>
-      {/* <Provider store={store}> */}
-        <App id={id}/>
-      {/* </Provider> */}
+      <Provider store={store}>
+        <App />
+      </Provider>
     </AppContainer>
     ,
     document.getElementById("root") as HTMLElement,
@@ -38,10 +47,10 @@ if (module.hot) {
     module.hot.accept();
 
     // reducers
-    // module.hot.accept("./reducers/index", () => {
-    //   const nextRootReducer = require("./reducers/index");
-    //   store.replaceReducer(nextRootReducer);
-    // });
+    module.hot.accept("./reducers/index", () => {
+      const nextRootReducer = require("./reducers/index");
+      store.replaceReducer(nextRootReducer);
+    });
 
     // // epics
     // module.hot.accept('../modules/root-epic', () => {
